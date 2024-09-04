57ef155a291250b70992c12c207ab7a6d7a4a6b6
Amani <amaniarnold08@gmail.com>
2024-09-04 10:34:25 +0300

    Answered the question

---
+++ user_input.py
@@ -0,0 +1,6 @@
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+user_location = input("Enter your location: ")
+
+
+print("Hello " + user_name + ",you are " + user_age + "years old and  live in " + user_location + '.')
